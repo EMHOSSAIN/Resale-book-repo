b6feb6fafee731af666b06482049396d58579e12
EMHOSSAIN <me288715@gmail.com>
2022-11-23 21:49:22 -0800

    created authonthication

--- src/Components/Share/Login/Login.jsx
+++ src/Components/Share/Login/Login.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handlelogin=event=>{
+        event.preventDefault()
+        const form=event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        
+        console.log(email,password)
+    }
+
+
     return (
-        <div>
-            <h1>Login page</h1>
+       <form onSubmit={handlelogin} >
+         <div className='mt-6'>
+            <div class="hero min-h-screen  bg-base-200">
+                <div class="hero-content  ">
+                   <div class="card flex-shrink-0 w-full max-w-md shadow-2xl  bg-base-100">
+                        <h1 class="text-2xl font-bold text-center pt-4">Login now!</h1>
+                        <div class="card-body">
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Email</span>
+                                </label>
+                                <input name='email' type="email" placeholder=" your email" class="input input-bordered" required />
+                            </div>
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Password</span>
+                                </label>
+                                <input name='password' type="password" placeholder=" your password" class="input input-bordered" required />
+                                <label class="label">
+                                   <p >Do not have Account?  < Link className='text-xl text-cyan-700' to='/signin' class="label-text-alt link link-hover">Sign Up</Link> </p>
+                                </label>
+                            </div>
+                            <div class="form-control mt-6">
+                                <button class="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+       </form>
     );
 };
 
--- src/Components/Share/Signup/SignUp.jsx
+++ src/Components/Share/Signup/SignUp.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
 
 const SignUp = () => {
+    const handlesignup=event=>{
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const image=form.image.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(name,image,email,password)
+    }
     return (
-        <div>
-            <h1>This is  home page</h1>
+       <form onSubmit={handlesignup}>
+         <div className='mt-6'>
+            <div class="hero min-h-screen bg-base-200">
+                <div class="hero-content ">
+                   <div class="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+                        <h1 class="text-2xl font-bold text-center pt-4">SignUp now!</h1>
+                        <div class="card-body">
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Name</span>
+                                </label>
+                                <input name='name' type="text" placeholder="email" class="input input-bordered" />
+                            </div>
+                            <div class="form-control">
+                                <label htmlFor='image' class="label">
+                                    <span class="label-text">Image</span>
+                                </label>
+                                <input name='image' type="file" class="input input-bordered pt-2" />
+                            </div>
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Email</span>
+                                </label>
+                                <input name='email' type="email" placeholder=" your email" class="input input-bordered" />
+                            </div>
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Password</span>
+                                </label>
+                                <input name='password' type="password" placeholder=" your password" class="input input-bordered" />
+                              </div>
+                               <div class="form-control mt-6">
+                                <button class="btn btn-primary">SignUp</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+       </form>
     );
 };
 
