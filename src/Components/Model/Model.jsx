23886a8708f0744dce20c373047d549b5468225d
EMHOSSAIN <me288715@gmail.com>
2022-11-26 04:41:23 -0800

    added model

--- src/Components/Model/Model.jsx
+++ src/Components/Model/Model.jsx
@@ -9,6 +9,7 @@ const Model = () => {
                     <label htmlFor="book modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
                     <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <p></p>
                 </div>
             </div>
         </div>
